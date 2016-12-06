3ac527f3bc577f36e66f47a805dfbdc541e2cf6c
TracySanFilipo <tracysanfilipo@gmail.com>
2016-12-06 17:01:23 -0500

    models created

--- question_app/models.py
+++ question_app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=35)
+    text = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag)
+    creator = models.ForeignKey(User)
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=500)
+    score = models.IntegerField(default=0)
+    question = models.ForeignKey(Question)
+    creator = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now=True)
+
+
+class Tag(models.Model):
+    name = CharField(max_length=15)
+
+
+class Vote(models.Model):
+    creator = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now=True)
+    answer = models.ForeignKey(Answer)
+    point = models.IntegerField(default=0)
